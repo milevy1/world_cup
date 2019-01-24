ecbe4ae8ae1812db6830c1bc12bdfe47273d3891
Matt Levy <milevy1@gmail.com>
2019-01-24 16:01:05 -0700

    Added world cup active players by position method

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -6,4 +6,21 @@ class WorldCup
     @teams = teams
   end
 
+  def active_players_by_position(position)
+    # Select players where position == position && team == active
+    players_array = []
+    @teams.each { |team|
+      if !team.eliminated
+        team.players_by_position(position).each{ |player|
+          players_array << player
+        }
+      end
+    }
+    return players_array
+  end
+
 end
+
+# For the `active_players_by_position` method,
+# an active player is a player that is on a team
+# that is not eliminated.
--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -42,4 +42,24 @@ class WorldCupTest < Minitest::Test
     assert_equal [france, croatia], world_cup.teams
   end
 
+  def test_active_players_by_position
+    france = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    france.add_player(mbappe)
+    france.add_player(pogba)
+
+    croatia = Team.new("Croatia")
+    modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    croatia.add_player(modric)
+    croatia.add_player(vida)
+
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    assert_equal [pogba, modric], world_cup.active_players_by_position("midfielder")
+    croatia.eliminated = true
+    assert_equal [pogba], world_cup.active_players_by_position("midfielder")
+  end
+
 end
