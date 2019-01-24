f1f67bd1ddd35d3e8309b30a8a48e0127d1d4a76
Matt Levy <milevy1@gmail.com>
2019-01-24 09:01:16 -0700

    Added team.eliminated atrribute and getter

--- lib/team.rb
+++ lib/team.rb
@@ -1,8 +1,14 @@
 class Team
   attr_reader :country
+  attr_accessor :eliminated
 
   def initialize(country)
     @country = country
+    @eliminated = false
+  end
+
+  def eliminated?
+    @eliminated
   end
 
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -11,4 +11,17 @@ class TeamTest < Minitest::Test
     assert_equal "France", team.country
   end
 
+  def test_eliminatied_starts_as_false
+    team = Team.new("France")
+
+    refute team.eliminated?
+  end
+
+  def test_eliminated_can_be_set_to_true
+    team = Team.new("France")
+    team.eliminated = true
+
+    assert team.eliminated?
+  end
+
 end
